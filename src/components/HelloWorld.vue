a31cc114c2bb4de87a69a2d2227836c963132dbd
Olivier Traveaux <olivier.traveaux@gmail.com>
2020-11-01 09:39:38 +0100

    data sample, index incrementer and shuffle function

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,33 +1,18 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h1>This is BS</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+      <li>
+        <p>{{cards[counter].question}}</p>
+        <p>{{cards[counter].answer}}</p>
+      </li>
+    </ul> 
+    <button v-on:click="counter++">+</button>
+    <button v-on:click="counter--">-</button>
+    <div>
+    <button v-on:click="shuffleDeck()">-</button>
+      
+    </div>
   </div>
 </template>
 
@@ -35,9 +20,48 @@
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
-  }
+    msg: String,
+  },
+  data: function () {
+    return {
+    counter: 1,
+    cards : [
+      {
+        question: 'Lorsque des chercheurs anglais ont inventé la première webcam, pour quoi a-t-elle été utilisée ?',
+        answer: 'La surveillance de leur machine à café.',
+        detail: 'Les chercheurs voulaient simplement vérifier s il restait du café dans la pièce d à côté.',
+        category: 'wtf'
+      },
+      {
+        question: 'Il y 100 millions d années, pourquoi aurait-il été impossible de prendre un bain de minuit ?',
+        answer: 'Parce que les jours ne duraient que 23 heures.',
+        detail: 'À sa formation, il y a 4 milliards d années, la journée terrestre ne durait que 6 heures.',
+        category: 'vie de la terre'
+      },
+            {
+        question: 'En 1979, le fermier Argentin Pedro Martin Ureta réalise un étrange hommage à sa femme décédée. Lequel ?',
+        answer: 'Il a fait pousser un forêt en forme de guitare.',
+        detail: 'La forêt et composée de 7000 arbres et mesure plus d un kilomètre de longueur.',
+        category: 'histoire'
+      },
+    ]
+    }
+  },
+  methods: {
+    shuffleDeck() {        
+      for(let i = this.cards.length - 1; i > 0; i--) {
+        let randomIndex = Math.floor(Math.random() * i);
+        
+        let temp = this.cards[i];
+        this.$set(this.cards, i, this.cards[randomIndex]);
+        this.$set(this.cards, randomIndex, temp);
+      }
+    },
+  },
+  computed: {
+    }
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
